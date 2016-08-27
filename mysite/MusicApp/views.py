b2ddeb1128013577d2b181714748154380dedc84
arpit1997 <solankiarpit1997@gmail.com>
2016-08-27 23:02:50 +0530

    created login page

--- mysite/MusicApp/views.py
+++ mysite/MusicApp/views.py
@@ -1,3 +1,25 @@
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def user_login(request):
+	if request.method == "POST":
+		username = request.POST.get("username")
+		passwd = request.POST.get("password")
+		user = authenticate(username=username, password=passwd)
+		if user is not None:
+			print("hello")
+			login(request, user)
+			return HttpResponse("success")
+		else:
+			# messages.error(request, "username and password did not match")
+			return render(request, "MusicApp/user_login.html")
+
+	else:
+		return render(request, "MusicApp/user_login.html")
+
+
+def home(request):
+	pass
