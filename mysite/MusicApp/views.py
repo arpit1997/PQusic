cf30b090dcd9e0aaaef6ad4c24398122c6b53f5a
arpit1997 <solankiarpit1997@gmail.com>
2016-09-01 00:19:31 +0530

    updated requirements.txt and added comments

--- mysite/MusicApp/views.py
+++ mysite/MusicApp/views.py
@@ -8,17 +8,23 @@ from django.shortcuts import render
 from Crypto.Cipher import AES, XOR
 import base64
 import os
+# encryption key for creating activation key
 secret_key = os.environ.get("encryption_key")
+# sender's email address in account verification email
 email_address = os.environ.get("email")
+# sender;s email password
 email_password = os.environ.get("password_a")
 
 
+# View to handle user login
 def user_login(request):
+	# if user has submitted a form for login
 	if request.method == "POST":
 		username = request.POST.get("username")
 		passwd = request.POST.get("password")
 		user = authenticate(username=username, password=passwd)
 		if user is not None:
+			# if user's email has been verfied then this will be true. default is false here
 			if user.is_active :
 				login(request, user)
 				return HttpResponse("success")
@@ -26,41 +32,53 @@ def user_login(request):
 			messages.error(request, "username and password did not match")
 			return render(request, "MusicApp/user_login.html")
 
+	# for a GET request
 	else:
 		return render(request, "MusicApp/user_login.html")
 
 
+# homepage of MusicApp yet to be implemented
 def home(request):
 	pass
 
 
+# view to handle user sign up
 def user_signup(request):
+	# if user has submitted a form
 	if request.method == "POST":
+		# extracting form's data submitted by user
 		username = request.POST.get("username")
 		email = request.POST.get("email")
 		passwd = request.POST.get("passwd")
 		first_name = request.POST.get("first_name")
 		last_name = request.POST.get("last_name")
+		# creating user
 		user = User.objects.create_user(username,email,passwd,first_name=first_name,last_name=last_name)
+		# custom save for creating non active user
 		custom_save(user)
 		activation_key = encrypt(secret_key,email)
+		# sending account verification mail
 		send_verification_mail(email, activation_key)
 		return HttpResponseRedirect(reverse("musicapp:activate"))
 
+	# for a GET request
 	else:
 		return render(request, "MusicApp/user_signup.html")
 
 
 def activate(request):
+	# if user has submitted a form for activation of an account
 	if request.method == "POST":
 		email = request.POST.get("email")
 		activation_key = request.POST.get("key")
+		# verifying thw activation key
 		decoded = decrypt(secret_key, activation_key)
 		decoded = decoded.decode("utf-8")
 		if email == decoded:
 			user = User.objects.get(email=email)
 			if user is None:
 				print("none user")
+			# activating the user
 			user.is_active = True
 			user.save()
 			return HttpResponse("account activated successfully")
@@ -73,21 +91,26 @@ def activate(request):
 # <---------------------------->
 # helper functions
 
+
+# custom save function for creating non active user
 def custom_save(user):
 	user.is_active = False
 	user.save()
 
 
+# encrypt function for creating activation keys
 def encrypt(key, plaintext):
 	cipher = XOR.new(key)
 	return base64.b64encode(cipher.encrypt(plaintext))
 
 
+# for decryption of activation keys
 def decrypt(key, ciphertext):
 	cipher = XOR.new(key)
 	return cipher.decrypt(base64.b64decode(ciphertext))
 
 
+# simple function for sending verification mails
 def send_verification_mail(email, activation_key):
 		server = smtplib.SMTP('smtp.gmail.com',587)
 		server.starttls()
